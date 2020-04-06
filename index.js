e8c09521f9a24ccb3444a4486d474303fc3f8592
Momenzi <16260069+Momenzi@users.noreply.github.com>
2020-04-06 22:59:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+var query = require('samp-query')
+const token = 'TOKEN';  // DiscordBot Token
+const PREFIX = '*'; // Prefix stavite po zelji
+var version = '1.0';
+var servers = {};
+var options = {
+    host: 'IP', // Vas IP
+    port: 7777 // PORT
+}
+
+bot.on("ready", () => {
+    bot.guilds.forEach((guild) => { 
+         let defaultChannel = "";
+         guild.channels.forEach((channel) => {
+               if(channel.type == "text" && defaultChannel == "") {
+               if(channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+                   defaultChannel = channel;
+               }
+               }
+         })
+         setInterval (function () 
+         {
+            query (options, (error, response) => {
+            if (error) {
+                console.error(error)
+            }
+            else {
+                let embed = new Discord.RichEmbed()
+                .setTitle(`${options.host}:${options.port} (${response.rules.version})`)
+                .setURL('https://ultimate-rpg.com/')
+                .setColor('#2ecc71')
+                .addField('Hostname', response.hostname)
+                .addField('Gamemode', response.gamemode)
+                .addField('Web', response.rules.weburl)
+                .addField('Online Players', `${response.online}/${response.maxplayers}`, true)
+                .addField('Hosted Tab', ':white_check_mark:', true)
+                .setFooter('Made by Momenzi#6717', 'https://www.sa-mp.com/samp_logo.png')
+                bot.channels.get(`ID KANALA`).send(embed) // STAVITE ID PO ZELJI
+            }
+        })
+         }, 10 * 360000);
+   })
+})
+ 
+bot.on('message', message => {
+    let args = message.content.substring(PREFIX.length).split(" ");
+    switch (args[0]){
+        case 'samp': // PRIMJER: *samp izbacuje poruku
+        query (options, (error, response) => {
+            if (error) {
+                console.error(error)
+            }
+            else {
+                let embed = new Discord.RichEmbed()
+                .setTitle(`${options.host}:${options.port} (${response.rules.version})`)
+                .setURL('https://ultimate-rpg.com/')
+                .setColor('#2ecc71')
+                .addField('Hostname', response.hostname)
+                .addField('Gamemode', response.gamemode)
+                .addField('Web', response.rules.weburl)
+                .addField('Hosted Tab', ':white_check_mark:', true)
+                .addField('Online Players', `${response.online}/${response.maxplayers}`, true)
+                .setFooter('Live data from SAMP server', 'https://www.sa-mp.com/samp_logo.png')
+                message.channel.send({embed});
+            }
+        })        
+        break;
+    }
+});
+bot.login(token);
